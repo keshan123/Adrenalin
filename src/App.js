177a287752f9e8e5c7fe2a13bae895441a1af083
keshan123 <keshan.peiris@gmail.com>
2019-04-01 10:43:32 +1100

    refactor app to a functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import ListingPage from './components/ListingPage/index';
@@ -6,19 +6,17 @@ import DetailsPage from './components/DetailsPage/index';
 import Navigation from './components/Navigation/index';
 import Footer from './components/Footer/index';
 
-class App extends Component {
-  render() {
-    return (
-      <Switch>
-        <Fragment>
-          <Navigation />
-          <Route exact path='/' component={ListingPage}/>
-          <Route path='/case-study/:id' component={DetailsPage}/>
-          <Footer />
-        </Fragment>
-      </Switch>
-    );
-  };
+function App() {
+  return (
+    <Switch>
+      <Fragment>
+        <Navigation />
+        <Route exact path='/' component={ListingPage}/>
+        <Route path='/case-study/:id' component={DetailsPage}/>
+        <Footer />
+      </Fragment>
+    </Switch>
+  );
 }
 
 export default App;
