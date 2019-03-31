0a89e04002d2a0b35d31c6a8af32698c881d48d0
keshan123 <keshan.peiris@gmail.com>
2019-03-31 18:24:23 +1100

    remove console logs

--- src/components/ListingPage.js
+++ src/components/ListingPage.js
@@ -12,6 +12,26 @@ import {
 
 let data = require('../feed/data.json');
 
+const ViewCaseSVG = () => (
+  <svg width="172px" height="11px" viewBox="0 0 172 11" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <g id="Pages" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g id="Listing-Page" transform="translate(-809.000000, -625.000000)">
+            <rect fill="#F5F5F5" x="0" y="0" width="1584" height="1420"></rect>
+            <g id="Component-/-Tiles-/-Desktop-/-4:3-" transform="translate(808.000000, 120.000000)" fill="#3852F7">
+                <g id="CTA-/-Desktop-/-Blue-/-Secondary" transform="translate(1.000000, 504.000000)">
+                    <text id="Secondary-Button" transform="translate(110.000000, 6.500000) rotate(-360.000000) translate(-110.000000, -6.500000) " fontFamily="Arial-BoldMT, Arial" fontSize="13" fontWeight="bold" linespacing="13" letterSpacing="0.189999998">
+                        <tspan x="48" y="12">VIEW CASE STUDY</tspan>
+                    </text>
+                    <g id="Colour/Interactive/1.-Electric-Blue" transform="translate(0.000000, 6.000000)" fillRule="evenodd">
+                        <rect id="Colour/Neutral/1.-Black" x="0" y="0" width="32" height="2"></rect>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
+  </svg>
+)
+
 class ListingPage extends Component {
   render() {
     return (
@@ -34,23 +54,7 @@ class ListingPage extends Component {
                   </TileDescription>
                   <ViewCaseStudy>
                     <Link to={`/case-study/${caseStudy.id}`}>
-                      <svg width="172px" height="11px" viewBox="0 0 172 11" version="1.1" xmlns="http://www.w3.org/2000/svg">
-                        <g id="Pages" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                            <g id="Listing-Page" transform="translate(-809.000000, -625.000000)">
-                                <rect fill="#F5F5F5" x="0" y="0" width="1584" height="1420"></rect>
-                                <g id="Component-/-Tiles-/-Desktop-/-4:3-" transform="translate(808.000000, 120.000000)" fill="#3852F7">
-                                    <g id="CTA-/-Desktop-/-Blue-/-Secondary" transform="translate(1.000000, 504.000000)">
-                                        <text id="Secondary-Button" transform="translate(110.000000, 6.500000) rotate(-360.000000) translate(-110.000000, -6.500000) " fontFamily="Arial-BoldMT, Arial" fontSize="13" fontWeight="bold" linespacing="13" letterSpacing="0.189999998">
-                                            <tspan x="48" y="12">VIEW CASE STUDY</tspan>
-                                        </text>
-                                        <g id="Colour/Interactive/1.-Electric-Blue" transform="translate(0.000000, 6.000000)" fillRule="evenodd">
-                                            <rect id="Colour/Neutral/1.-Black" x="0" y="0" width="32" height="2"></rect>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </g>
-                      </svg>
+                      <ViewCaseSVG />
                     </Link>
                    </ViewCaseStudy>
                 </PreviewTile>
